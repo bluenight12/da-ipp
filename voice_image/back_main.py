cae7b62415d4c7278aa1bed5c958948de673286a
bluenight12 <ekehd258@gmail.com>
2024-03-21 11:13:46 +0900

    git ignore

--- voice_image/back_main.py
+++
@@ -1,9 +0,0 @@
-import back_recognize_text_module
-import back_translate_korean_to_english
-from text2background_image_fin import TextToImageGenerator
-from text2background_image_fin import *
-
-back_recognize_text_module.back_recognize_and_save_text()
-back_translate_korean_to_english.back_translate_korean_to_english("back.txt", "bck_eng.txt")
-
-TextToImageGenerator.generate_image("bck_eng.txt")
